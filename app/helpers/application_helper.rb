66335899764d0cefa5f578b757d0263393c099bd
Michael Basmanov <puglet5@mail.ru>
2021-07-22 22:17:54 +0300

    Reenabled auth_helper from prev commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
-  # def auth_helper(style = "")
-  #   if current_user.is_a?(GuestUser)
-  #     (link_to "Login", new_user_session_path, class: style) +
-  #     (link_to " Register", new_user_registration_path, class: style)
-  #   else
-  #     link_to "Logout", destroy_user_session_path, method: :delete, class: style
-  #   end
-  # end
+  def auth_helper(style = "")
+    if current_user.is_a?(GuestUser)
+      (link_to "Login", new_user_session_path, class: style) +
+      (link_to " Register", new_user_registration_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
+  end
 
   def set_copyright
     ViewTool::Renderer.copyright "Michael Basmanov", "All rights reserved"
