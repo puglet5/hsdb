ef33dcb7830b6f701364fc448e4ca49d3ad60667
Michael Basmanov <puglet5@mail.ru>
2021-07-22 23:33:20 +0300

    Minor fixes

--- app/controllers/discussions_controller.rb
+++ app/controllers/discussions_controller.rb
@@ -1,16 +1,18 @@
 class DiscussionsController < ApplicationController
   before_action :set_discussion, only: [:show, :edit, :update, :destroy]
+  before_action :find_categories, only: [:index, :show, :new, :edit]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :find_categories, only: [:index, :new, :show, :edit]
-  before_action :list_discussions, only: [:index, :show]
-  # access all: [:index, :show, :new, :edit, :create, :update, :destroy], user: :all
 
   # GET /discussions
+  # GET /discussions.json
   def index
+    @discussions = Discussion.all.order("created_at desc")
   end
 
   # GET /discussions/1
+  # GET /discussions/1.json
   def show
+    @discussions = Discussion.all.order("created_at desc")
   end
 
   # GET /discussions/new
@@ -23,29 +25,44 @@ class DiscussionsController < ApplicationController
   end
 
   # POST /discussions
+  # POST /discussions.json
   def create
     @discussion = current_user.discussions.build(discussion_params)
 
-    if @discussion.save
-      redirect_to @discussion, notice: "Discussion was successfully created."
-    else
-      render :new
+    respond_to do |format|
+      if @discussion.save
+        format.html { redirect_to @discussion, notice: "Discussion was successfully created." }
+        format.json { render :show, status: :created, location: @discussion }
+      else
+        byebug
+        format.html { render :new }
+        format.json { render json: @discussion.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # PATCH/PUT /discussions/1
+  # PATCH/PUT /discussions/1.json
   def update
-    if @discussion.update(discussion_params)
-      redirect_to @discussion, notice: "Discussion was successfully updated."
-    else
-      render :edit
+    respond_to do |format|
+      if @discussion.update(discussion_params)
+        format.html { redirect_to @discussion, notice: "Discussion was successfully updated." }
+        format.json { render :show, status: :ok, location: @discussion }
+      else
+        format.html { render :edit }
+        format.json { render json: @discussion.errors, status: :unprocessable_entity }
+      end
     end
   end
 
   # DELETE /discussions/1
+  # DELETE /discussions/1.json
   def destroy
     @discussion.destroy
-    redirect_to discussions_url, notice: "Discussion was successfully destroyed."
+    respond_to do |format|
+      format.html { redirect_to discussions_url, notice: "Discussion was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
@@ -59,12 +76,8 @@ class DiscussionsController < ApplicationController
     @categories = Category.all.order("created_at desc")
   end
 
-  def list_discussions
-    @discussions = Discussion.all.order("created_at desc")
-  end
-
-  # Only allow a trusted parameter "white list" through.
+  # Never trust parameters from the scary internet, only allow the white list through.
   def discussion_params
-    params.require(:discussion).permit(:title, :content)
+    params.require(:discussion).permit(:title, :content, :category_id)
   end
 end
