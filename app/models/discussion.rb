a742e21643948ac9c12f7614df925597025de06e
Michael Basmanov <puglet5@mail.ru>
2021-07-23 20:45:04 +0300

    Added admin roleand partially edited views based on user role

--- app/models/discussion.rb
+++ app/models/discussion.rb
@@ -4,4 +4,6 @@ class Discussion < ApplicationRecord
   has_many :replies, dependent: :destroy
 
   validates :title, :content, presence: true
+
+  resourcify
 end
--- db/schema.rb
+++ db/schema.rb
@@ -83,4 +83,5 @@ ActiveRecord::Schema.define(version: 2021_07_23_130008) do
     t.index ["user_id", "role_id"], name: "index_users_roles_on_user_id_and_role_id"
     t.index ["user_id"], name: "index_users_roles_on_user_id"
   end
+
 end
