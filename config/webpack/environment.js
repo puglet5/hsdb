bab61c9853d9c12c9dee0672d62e0ff28d7ad596
Michael Basmanov <puglet5@mail.ru>
2022-01-15 16:29:26 +0300

    Moved to webpacker 6

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,24 @@
-const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+environment.plugins.prepend('Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'],
+    toastr: 'toastr/toastr',
+    ApexCharts: ['apexcharts', 'default'],
+    underscore: ['underscore', 'm'],
+    Rails: ['@rails/ujs']
+  })
+)
+
+Encore.configureDevServerOptions(options => {
+  options.allowedHosts = 'all';
+  options.https = {
+    cert: '/etc/apache2/ssl/domain.dev/fullchain.pem',
+    key: '/etc/apache2/ssl/domain.dev/privkey.pem',
+  };
+  
+  delete options.client.host;
+})
 
 module.exports = environment
