9edb9324866e1e0be0e726e395ecb011dc40d5be
Michael Basmanov <puglet5@mail.ru>
2022-01-15 20:18:01 +0300

    Implemented localized routes scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'users/show'
-  resources :categories
-  resources :users
-  resources :discussions do
-    resources :replies
-  end
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
-  resources :uploads
-  get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
-  get 'welcome', to: 'pages#welcome'
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    get 'users/show'
+    resources :categories
+    resources :users
+    resources :discussions do
+      resources :replies
+    end
+    devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+    resources :uploads
+    get 'about', to: 'pages#about'
+    get 'contact', to: 'pages#contact'
+    get 'welcome', to: 'pages#welcome'
 
-  namespace :admin do
-    resources :users, only: %i[index]
-  end
+    namespace :admin do
+      resources :users, only: %i[index]
+    end
 
-  root to: 'pages#home'
+    root to: 'pages#home'
+  end
 end
