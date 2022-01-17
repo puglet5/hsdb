4bcfa62566a29a9d28b175475cb964b5bc6bd2d6
Michael Basmanov <puglet5@mail.ru>
2022-01-17 19:41:04 +0300

    Added delete buttons, refactored old code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,12 +37,12 @@ module ApplicationHelper
     [
       {
         url: new_user_session_path,
-        title: t('auth.login')
+        title: t('auth.login'),
       },
       {
         url: new_user_registration_path,
-        title: t('auth.register')
-      }
+        title: t('auth.register'),
+      },
     ]
   end
 
@@ -50,20 +50,20 @@ module ApplicationHelper
     [
       {
         url: root_path,
-        title: t('nav.home')
+        title: t('nav.home'),
       },
       {
         url: about_path,
-        title: t('nav.about')
+        title: t('nav.about'),
       },
       {
         url: uploads_path,
-        title: t('nav.uploads')
+        title: t('nav.uploads'),
       },
       {
         url: discussions_path,
-        title: t('nav.forum')
-      }
+        title: t('nav.forum'),
+      },
     ]
   end
 
@@ -81,8 +81,6 @@ module ApplicationHelper
     'active' if current_page? path
   end
 
-  # require 'redcarpet/render_strip'
-
   def has_role?(role)
     current_user&.has_role?(role)
   end
@@ -93,28 +91,4 @@ module ApplicationHelper
       "#{Time.zone.now.year} | #{msg}"
     end
   end
-
-  # # Markdown Helpers
-  # class CodeRayify < Redcarpet::Render::HTML
-  #   def block_code(code, language)
-  #     CodeRay.scan(code, language).div
-  #   end
-  # end
-
-  # def markdown(text)
-  #   coderayified = CodeRayify.new(:filter_html => true, :hard_wrap => true)
-  #   options = {
-  #     fenced_code_blocks: true,
-  #     no_intra_emphasis: true,
-  #     autolink: true,
-  #     lax_html_blocks: true,
-  #   }
-  #   markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-  #   markdown_to_html.render(text).html_safe
-  # end
-
-  # def strip_markdown(string)
-  #   markdown_to_plain_text = Redcarpet::Markdown.new(Redcarpet::Render::StripDown)
-  #   markdown_to_plain_text.render(string).html_safe
-  # end
 end
