ffa9519956fa029d2de6e3ce3f478362e402fac3
Michael Basmanov <puglet5@mail.ru>
2022-01-18 00:07:34 +0300

    Upload owner validations and fixes

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -48,6 +48,9 @@ class UploadsController < ApplicationController
 
   # PATCH/PUT /uploads/1 or /uploads/1.json
   def update
+    if @upload.user.nil?
+      @upload.user = current_user
+    end
     respond_to do |format|
       if @upload.update(upload_params.reject { |k| k['images'] })
         if upload_params[:images].present?
