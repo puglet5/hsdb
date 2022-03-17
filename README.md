4abb0f62e1b36b6d8c6300d03514dfa965ea7bc2
Michael Basmanov <puglet5@mail.ru>
2022-03-17 23:33:19 +0300

    New OS setup

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-graphicsmagick rvm postgres nodejs bundler libpqdev postgres db:setup users yarn webpacker:install
+graphicsmagick rvm postgres nodejs bundler libpqdev postgres db:setup users yarn webpacker:install rails dev:cache redis check postgresuser graphviz solargraph foreman
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
-# frozen_string_literal: true
+# # frozen_string_literal: true
 
-Faker::Config.locale = 'en'
+# Faker::Config.locale = 'en'
 
-10.times do
-  User.create!(first_name: Faker::Name.first_name,
-               second_name: Faker::Name.second_name,
-               email: Faker::Internet.email,
-               password: '123456')
-end
+# 10.times do
+#   User.create!(first_name: Faker::Name.first_name,
+#                second_name: Faker::Name.second_name,
+#                email: Faker::Internet.email,
+#                password: '123456')
+# end
 
-10.times do |_x|
-  Upload.create!(title: Faker::Lorem.sentences(number: 1),
-                 description: Faker::Lorem.paragraph(sentence_count: 2),
-                 body: Faker::Lorem.paragraph(sentence_count: 5),
-                 user_id: 2)
-end
+# 10.times do |_x|
+#   Upload.create!(title: Faker::Lorem.sentences(number: 1),
+#                  description: Faker::Lorem.paragraph(sentence_count: 2),
+#                  body: Faker::Lorem.paragraph(sentence_count: 5),
+#                  user_id: 2)
+# end
 
-10.times do
-  Tag.create!(title: Faker::Hipster.word)
-end
+# 10.times do
+#   Tag.create!(title: Faker::Hipster.word)
+# end
 
-Role.create!(name: 'admin')
+# Role.create!(name: 'admin')
