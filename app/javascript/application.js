180675b5429eac8f2eb3c064641a11f5cd1ddadb
Michael Basmanov <puglet5@mail.ru>
2022-06-27 05:43:06 +0300

    further cleanup, tailwind is working

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,24 +3,24 @@ global.Rails = Rails;
 
 import * as ActiveStorage from "@rails/activestorage";
 import "./channels";
-import "./controllers";
+// import "./controllers";
 
 import "./packs/direct_upload";
+
 // import "./dataconfirm"
 // import "./styles/application.scss";
+// import "beercss/src/cdn/beer"
 
-import "beercss/src/cdn/beer"
 import "trix"
 
 import "@rails/actiontext";
 
 var Turbolinks = require("turbolinks")
 ActiveStorage.start();
-
 Turbolinks.start()
 
-require("jquery");
-require("@rails/actiontext")
+// require("jquery");
+// require("@rails/actiontext")
 
 // require.context('img', true)
 
