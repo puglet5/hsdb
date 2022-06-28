103f2736efbb3aeeaec1240fc1eccfd8449b495d
Michael Basmanov <puglet5@mail.ru>
2022-06-28 11:41:16 +0300

    dismissable alerts (one type)

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,7 @@
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
-require("./channels");
+// require("./channels");
 
 import "./packs/direct_upload";
 import "trix"
