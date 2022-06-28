523bc63e9552cd76704ba8b7a5a2f2fdaa8f3449
Michael Basmanov <puglet5@mail.ru>
2022-06-28 10:20:38 +0300

    added itmo colors, rewrote nav, styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   content: [
     './app/views/**/*.html.erb',
@@ -12,6 +14,47 @@ module.exports = {
   theme: {
     container: {
       center: true
+    },
+    colors: {
+      itmo_primary: {
+        500: "#2251a3",
+        400: "#2251a3",
+        300: "#8fa7d0",
+        200: "#c6d2e7"
+      },
+      itmo_secondary: {
+        500: "#eb1946",
+        400: "#f05274",
+        300: "#f48ba1",
+        200: "#f9c4d0"
+      },
+      itmo_misc_1: {
+        500: "#f57e25",
+      },
+      itmo_misc_2: {
+        500: "#3fb1e5",
+      },
+      itmo_misc_3: {
+        500: "#96c93d",
+      },
+      itmo_misc_4: {
+        500: "#22ac9b",
+      },
+      itmo_misc_5: {
+        500: "#4bd932",
+      },
+      itmo_misc_6: {
+        500: "#ffd600",
+      },
+      itmo_misc_7: {
+        500: "#eee809",
+      },
+      itmo_misc_8: {
+        500: "#ef3ed2",
+      },
+      itmo_misc_9: {
+        500: "#00914d",
+      },
     }
   }
 }
