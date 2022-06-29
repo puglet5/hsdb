0cd0767e0410a1b012d6ba197a1c625f630a543a
Michael Basmanov <puglet5@mail.ru>
2022-06-29 11:37:06 +0300

    addressed https://github.com/puglet5/hsdb/issues/114

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,5 +10,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     update_attrs = [:avatar, :first_name, :last_name, :organization, :password, :password_confirmation, :current_password, { role_ids: [] }]
     devise_parameter_sanitizer.permit :account_update, keys: update_attrs
+    devise_parameter_sanitizer.permit :sign_up, keys: update_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
   end
 end
