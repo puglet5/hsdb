8abd6f8c11e76bc3f7c0024490f704d14d651061
Michael Basmanov <puglet5@mail.ru>
2022-07-01 11:25:17 +0300

    misc styling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,18 +1,10 @@
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
-// require("./channels");
+
 
 import "./packs/direct_upload";
 import "trix"
 import "flowbite/dist/flowbite.js"
 import 'tw-elements';
 import "@rails/actiontext";
-// require("jquery");
-// require("@rails/actiontext")
-
-// require.context('img', true)
-
-// document.addEventListener("turbolinks:load", () => {
-//   ui();
-// })
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require('tailwindcss/colors')
 
 module.exports = {
-  important: true,
+  darkMode: 'class',
   content: [
     './app/views/**/*.html.erb',
     './app/helpers/**/*.rb',
