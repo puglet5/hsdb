f685403caf9ece20e8b8f8e1eada77eb65ed75df
Michael Basmanov <puglet5@mail.ru>
2022-07-07 13:33:14 +0300

    installed propshaft and turbo-rails

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,13 +1,11 @@
-require("@rails/ujs").start();
-require("turbolinks").start();
 require("@rails/activestorage").start();
-
 
 import "./packs/direct_upload";
 import "trix"
 import "flowbite/dist/flowbite.js"
 import 'tw-elements';
 import "@rails/actiontext";
+import "@hotwired/turbo-rails"
 
 
 
