b55804e839b875ef4b0b4811b84331b0eb530e2a
Michael Basmanov <puglet5@mail.ru>
2022-07-07 14:02:33 +0300

    misc styling and js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -8,40 +8,37 @@ import "@rails/actiontext";
 import "@hotwired/turbo-rails"
 
 
-
-//////// Back to top button
-
-
-// Get the button
-let mybutton = document.getElementById('btn-back-to-top');
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    mybutton.style.display = 'block';
-  } else {
-    mybutton.style.display = 'none';
-  }
-}
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener('click', backToTop);
-
-function backToTop() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
 document.addEventListener("turbo:before-visit", e => {
   window.MiniProfilerContainer = document.querySelector('body > .profiler-results')
-  if(!e.defaultPrevented) window.MiniProfiler.pageTransition()
+  if (!e.defaultPrevented) window.MiniProfiler.pageTransition()
 })
 
 document.addEventListener("turbo:load", e => {
-  if(window.MiniProfilerContainer) {
+  if (window.MiniProfilerContainer) {
     document.body.appendChild(window.MiniProfilerContainer)
+  }
+
+
+  // Get the button
+  let mybutton = document.getElementById('btn-back-to-top');
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+      mybutton.style.display = 'block';
+    } else {
+      mybutton.style.display = 'none';
+    }
+  }
+  // When the user clicks on the button, scroll to the top of the document
+  mybutton.addEventListener('click', backToTop);
+
+  function backToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
 })
