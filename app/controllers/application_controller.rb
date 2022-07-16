7c378eead26b49dee89a0bc1a7f4c02d3cd988d5
Michael Basmanov <puglet5@mail.ru>
2022-07-17 00:03:52 +0300

    404 error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
   include PublicActivity::StoreController
   include Internationalization
   include Authorization
+  include ErrorHandling
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
 
   def configure_permitted_parameters
     update_attrs = [:avatar, :first_name, :last_name, :organization, :password, :password_confirmation, :current_password, { role_ids: [] }]
@@ -13,4 +17,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :sign_up, keys: update_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: %i[login password]
   end
+
 end
