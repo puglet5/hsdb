5681a60c1bd505b29c3b75a8374fb04279169d80
Michael Basmanov <puglet5@mail.ru>
2022-07-19 08:03:45 +0300

    reply controller refactoring

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,8 +2,8 @@
 
 class RepliesController < ApplicationController
   before_action :authenticate_user!
-  # before_action :set_reply, only: %i[edit update show destroy]
   before_action :set_discussion, only: %i[create edit show update destroy]
+  before_action :set_reply, only: %i[edit update destroy]
 
   def create
     @reply = @discussion.replies.create(params[:reply].permit(:reply, :discussion_id))
@@ -19,18 +19,13 @@ class RepliesController < ApplicationController
   def new; end
 
   def destroy
-    @reply = @discussion.replies.find(params[:id])
     @reply.destroy
     redirect_to discussion_path(@discussion), status: :see_other
   end
 
-  def edit
-    @discussion = Discussion.find(params[:discussion_id])
-    @reply = @discussion.replies.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @reply = @discussion.replies.find(params[:id])
     if @reply.update(reply_params)
       redirect_to discussion_path(@discussion)
     else
@@ -47,7 +42,7 @@ class RepliesController < ApplicationController
   end
 
   def set_reply
-    @reply = Reply.find(params[:id])
+    @reply = @discussion.replies.find(params[:id])
   end
 
   def reply_params
