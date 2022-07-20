37d7ed779f66162170dcc6531b3e5b823dc76f81
Michael Basmanov <puglet5@mail.ru>
2022-07-20 23:08:30 +0300

    set up automatic role switching

--- db/rsdb_schema.rb
+++ db/rsdb_schema.rb
@@ -1,4 +1,5 @@
 ActiveRecord::Schema[7.0].define(version: 20_220_720_163_907) do
+  # These are extensions that must be enabled in order to support this database
   enable_extension 'plpgsql'
 
   create_table 'spectra', force: :cascade do |t|
