3d84a586c64943725759d20c36f29842756ff4cb
Michael Basmanov <puglet5@mail.ru>
2022-07-24 00:51:51 +0300

    styled spectrum show page

---
+++ app/policies/spectrum_policy.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+class SpectrumPolicy < ApplicationPolicy
+  def create?
+    !user.guest?
+  end
+
+  def update?
+    user.has_role?(:admin) || user.author?(record)
+  end
+
+  def destroy?
+    user.has_role?(:admin) || user.author?(record)
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def images?
+    true
+  end
+end
