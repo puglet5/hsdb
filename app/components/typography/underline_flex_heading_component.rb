007055a109ccd23067619ef775432375f1bd8953
Michael Basmanov <puglet5@mail.ru>
2022-07-29 18:07:18 +0300

    user edit page component refactoring and submit component fix

--- app/components/typography/underline_flex_heading_component.rb
+++ app/components/typography/underline_flex_heading_component.rb
@@ -1,7 +1,9 @@
 # frozen_string_literal: true
 
-class Typography::UnderlineFlexHeadingComponent < ViewComponent::Base
-  def initialize(cls: '')
-    @cls = cls
+module Typography
+  class UnderlineFlexHeadingComponent < ViewComponent::Base
+    def initialize(cls: '')
+      @cls = cls
+    end
   end
 end
