4bb192391dd7bd03f9ef6fd2d880df8ccbcad5bd
Michael Basmanov <puglet5@mail.ru>
2022-08-02 23:37:11 +0300

    upload model spec and factory

--- spec/factories/uploads.rb
+++ spec/factories/uploads.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :upload do
-    
+    title { 'test title' }
+    description { 'test description' }
+    body { 'test body' }
+    user_id factory: :user
+    metadata { '{"test_key": "test_value"}' }
   end
 end
--- spec/models/upload_spec.rb
+++ spec/models/upload_spec.rb
@@ -6,13 +6,18 @@ RSpec.describe Upload, type: :model do
   let(:valid_attributes) do
     { 'title' => 'Test Upload',
       'user_id' => user.id,
-      'metadata' => '{}',
-      'status' => 'draft',
       'body' => '<p> test upload body </p>',
-      'description' => 'Test upload description' }
+      'description' => 'Test upload description',
+      'metadata' => '{}' }
   end
 
-  fdescribe 'Presence validations' do
+  let(:valid_upload) { build(:upload, user: user) }
+
+  before(:each) do
+    valid_upload.images.delete_all
+  end
+
+  fdescribe 'Field presence validations' do
     it 'is valid with valid attributes' do
       upload = Upload.new valid_attributes
       expect(upload).to be_valid
@@ -32,5 +37,69 @@ RSpec.describe Upload, type: :model do
       upload = Upload.new(title: 'test title', description: 'test description', body: nil)
       expect(upload).to_not be_valid
     end
+
+    it 'has an existing associated user' do
+      expect(valid_upload.user_id).not_to eq(nil)
+    end
+
+    it 'has a parsed metadata' do
+      valid_upload.save!
+      expect(valid_upload.metadata).to be_a(Hash).or eq('{}')
+    end
+
+    it 'has a default status of draft' do
+      valid_upload.save!
+      expect(valid_upload.status).to eq('draft')
+    end
+
+    it 'has no attachments by default' do
+      valid_upload.save!
+      expect(valid_upload.images.any?).to eq(false)
+      expect(valid_upload.thumbnail.attached?).to eq(false)
+      expect(valid_upload.documents.any?).to eq(false)
+    end
+  end
+
+  describe 'ActiveStorage attachment type validations' do
+    shared_examples 'single image of valid types' do |filetype|
+      it "accepts image/#{filetype}" do
+        valid_upload.images.attach(io: File.open(file_fixture("test.#{filetype}")), filename: "test.#{filetype}", content_type: "image/#{filetype}")
+        valid_upload.save!
+        expect(valid_upload.errors.messages[:images]).to eq []
+        expect(valid_upload.images.count).to eq(1)
+        expect(valid_upload.images.first.persisted?).to eq(true)
+      end
+    end
+
+    include_examples 'single image of valid types', 'jpeg'
+    include_examples 'single image of valid types', 'png'
+    include_examples 'single image of valid types', 'gif'
+
+    context 'multiple images of valid types' do
+      it 'accepts 2 jpeg images' do
+        2.times do
+          valid_upload.images.attach(io: File.open(file_fixture('test.jpeg')), filename: 'test.jpeg', content_type: 'image/jpeg')
+        end
+        expect(valid_upload.images.count).to eq(2)
+      end
+    end
+
+    context 'invalid files and file types' do
+      it 'doesn\'t accept image/tiff' do
+        valid_upload.images.attach(io: File.open(file_fixture('test.tiff')), filename: 'test.tiff', content_type: 'image/tiff')
+        valid_upload.save
+        expect(valid_upload.errors.messages[:images]).to eq ['is not a valid file format']
+        expect(valid_upload.images.first.persisted?).to eq(false)
+      end
+      it 'doesn\'t accept text/csv' do
+        valid_upload.images.attach(io: File.open(file_fixture('test.csv')), filename: 'test.csv', content_type: 'text/csv')
+        valid_upload.save
+        expect(valid_upload.errors.messages[:images]).to eq ['is not a valid file format']
+        expect(valid_upload.images.first.persisted?).to eq(false)
+      end
+    end
+  end
+
+  fdescribe 'ActiveStorage attachments' do
   end
 end
