d23281a90b43c5b58612c42e885ad2987b303957
Michael Basmanov <puglet5@mail.ru>
2022-08-03 17:08:57 +0300

    categories actions turbo statuses

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -33,14 +33,10 @@ class CategoriesController < ApplicationController
   def create
     @category = Category.new(category_params)
 
-    respond_to do |format|
-      if @category.save
-        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
-        format.json { render :show, status: :created, location: @category }
-      else
-        format.html { render :new }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
-      end
+    if @category.save
+      redirect_to categories_path, notice: 'Category was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
     end
   end
 
@@ -49,7 +45,7 @@ class CategoriesController < ApplicationController
     if @category.update(category_params)
       redirect_to categories_path, notice: 'Category was successfully updated.'
     else
-      render :edit
+      render :edit, status: :unprocessable_entity
     end
   end
 
