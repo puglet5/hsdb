7053e025e554e99ae73031f2752fc4a59087d934
Michael Basmanov <puglet5@mail.ru>
2022-08-04 03:25:31 +0300

    explicitly set some options in `esbuild.config.js`

--- esbuild.config.js
+++ esbuild.config.js
@@ -6,6 +6,11 @@ const rails = require("esbuild-rails")
 require("esbuild").build({
   entryPoints: ["application.js"],
   bundle: true,
+  minify: true,
+  platform: 'browser',
+  target: [
+    'es2015',
+  ],
   outdir: path.join(process.cwd(), "app/assets/builds"),
   absWorkingDir: path.join(process.cwd(), "app/javascript"),
   watch: process.argv.includes("--watch"),
