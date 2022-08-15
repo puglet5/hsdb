82e2829c8247cb2704cbf01724ec26f0266f0cfe
Michael Basmanov <puglet5@mail.ru>
2022-08-15 08:57:34 +0300

    add uniqueness constraint on material names

--- app/models/material.rb
+++ app/models/material.rb
@@ -3,4 +3,6 @@
 class Material < ApplicationRecord
   has_many :upload_materials, dependent: :destroy
   has_many :uploads, through: :upload_materials
+
+  validates :name, presence: true, uniqueness: true
 end
