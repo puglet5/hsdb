f495854982b04a31d62a7365c53e5a3be76c169b
Michael Basmanov <puglet5@mail.ru>
2022-09-05 01:53:18 +0300

    add type column to images

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,15 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: images
+#
+#  id         :bigint           not null, primary key
+#  upload_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  type       :integer          default(0)
+#
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
