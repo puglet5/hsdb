278248e1c61bc36d6498f50067e17a1ec557a91d
Michael Basmanov <puglet5@mail.ru>
2022-09-07 22:55:51 +0300

    fix generate_variants job

--- app/jobs/generate_variants_job.rb
+++ app/jobs/generate_variants_job.rb
@@ -4,8 +4,10 @@ class GenerateVariantsJob < ApplicationJob
   queue_as :default
 
   def perform(id)
-    Upload.find_by(id: id)&.images&.each do |image|
-      next unless image&.representable?
+    upload = Upload.find id
+
+    upload&.image_attachments&.each do |image|
+      next unless image.representable?
 
       preview_variant = image.variant(resize: '400x300^', crop: '400x300+0+0')
       preview_variant.process unless preview_variant.processed?
