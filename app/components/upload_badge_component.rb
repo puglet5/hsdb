648537416497fa0d51a3344eb6a8485a56def0b8
Michael Basmanov <puglet5@mail.ru>
2022-09-22 16:40:30 +0300

    redesign upload index page

---
+++ app/components/upload_badge_component.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class UploadBadgeComponent < ViewComponent::Base
+  attr_reader :status
+
+  def initialize(status: '', cls: '')
+    @status = status
+    @cls = cls
+  end
+
+  STATUSES = {
+    'active' => 'text-sky-800 bg-sky-100',
+    'draft' => 'text-yellow-800 bg-yellow-100',
+    'archived' => 'text-gray-800 bg-gray-100'
+  }.freeze
+
+  def color(status)
+    STATUSES[status]
+  end
+end
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,13 @@
 /* eslint-disable */
-// const colors = require('tailwindcss/colors')
 
 module.exports = {
   darkMode: "class",
-  mode: "jit",
   content: [
-    "./app/views/**/*.html.erb",
-    "./app/helpers/**/*.rb",
-    "./app/assets/stylesheets/**/*.css",
-    "./app/javascript/**/*.js",
+    './app/views/**/*.html.erb',
+    './app/components/*',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
   ],
   plugins: [
     require("flowbite/plugin"),
@@ -34,6 +33,65 @@ module.exports = {
         "2/3": "66%",
         "4/5": "80%",
       },
+      colors: {
+        primary: {
+          100: "#d3dced",
+          200: "#a7b9da",
+          300: "#7a97c8",
+          400: "#4e74b5",
+          500: "#2251a3",
+          600: "#1b4182",
+          700: "#143162",
+          800: "#0e2041",
+          900: "#071021"
+        },
+        secondary: {
+          100: "#fbd1da",
+          200: "#f7a3b5",
+          300: "#f37590",
+          400: "#ef476b",
+          500: "#eb1946",
+          600: "#bc1438",
+          700: "#8d0f2a",
+          800: "#5e0a1c",
+          900: "#2f050e"
+        },
+        itmo_misc_1: {
+          100: "#fde5d3",
+          200: "#fbcba8",
+          300: "#f9b27c",
+          400: "#f79851",
+          500: "#f57e25",
+          600: "#c4651e",
+          700: "#934c16",
+          800: "#62320f",
+          900: "#311907"
+        },
+        itmo_misc_2: {
+          500: "#3fb1e5",
+        },
+        itmo_misc_3: {
+          500: "#96c93d",
+        },
+        itmo_misc_4: {
+          500: "#22ac9b",
+        },
+        itmo_misc_5: {
+          500: "#4bd932",
+        },
+        itmo_misc_6: {
+          500: "#ffd600",
+        },
+        itmo_misc_7: {
+          500: "#eee809",
+        },
+        itmo_misc_8: {
+          500: "#ef3ed2",
+        },
+        itmo_misc_9: {
+          500: "#00914d",
+        },
+      }
     },
     fontFamily: {
       // 'sans': 'ALS Schlangesans, Calibri, Muller, sans-serif',
@@ -45,64 +103,5 @@ module.exports = {
     container: {
       center: true
     },
-    colors: {
-      primary: {
-        100: "#d3dced",
-        200: "#a7b9da",
-        300: "#7a97c8",
-        400: "#4e74b5",
-        500: "#2251a3",
-        600: "#1b4182",
-        700: "#143162",
-        800: "#0e2041",
-        900: "#071021"
-      },
-      secondary: {
-        100: "#fbd1da",
-        200: "#f7a3b5",
-        300: "#f37590",
-        400: "#ef476b",
-        500: "#eb1946",
-        600: "#bc1438",
-        700: "#8d0f2a",
-        800: "#5e0a1c",
-        900: "#2f050e"
-      },
-      itmo_misc_1: {
-        100: "#fde5d3",
-        200: "#fbcba8",
-        300: "#f9b27c",
-        400: "#f79851",
-        500: "#f57e25",
-        600: "#c4651e",
-        700: "#934c16",
-        800: "#62320f",
-        900: "#311907"
-      },
-      itmo_misc_2: {
-        500: "#3fb1e5",
-      },
-      itmo_misc_3: {
-        500: "#96c93d",
-      },
-      itmo_misc_4: {
-        500: "#22ac9b",
-      },
-      itmo_misc_5: {
-        500: "#4bd932",
-      },
-      itmo_misc_6: {
-        500: "#ffd600",
-      },
-      itmo_misc_7: {
-        500: "#eee809",
-      },
-      itmo_misc_8: {
-        500: "#ef3ed2",
-      },
-      itmo_misc_9: {
-        500: "#00914d",
-      },
-    }
   }
 }
