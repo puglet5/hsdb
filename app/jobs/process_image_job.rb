426cab9a394bd55a3fb5d5b7eabffc68f07fb9dc
Michael Basmanov <puglet5@mail.ru>
2022-09-28 18:12:29 +0300

    make rubocop happy

--- app/jobs/process_image_job.rb
+++ app/jobs/process_image_job.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
+# rubocop:disable Rails/SkipsModelValidations
+
 class ProcessImageJob < ApplicationJob
   queue_as :default
 
   def perform(obj)
-    unless obj.image.variant(:thumbnail).key
-      obj.image.variant(:thumbnail).processed
-      obj.touch
-    end
+    return if obj.image.variant(:thumbnail).key
+
+    obj.image.variant(:thumbnail).processed
+    obj.touch
   end
 end
+
+# rubocop:enable Rails/SkipsModelValidations
