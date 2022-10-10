ede08b503ed4eeba03a1fb7d5413a160396facf9
Michael Basmanov <puglet5@mail.ru>
2022-10-11 02:59:25 +0300

    add tabs controller

---
+++ app/javascript/controllers/tabs_controller.js
@@ -0,0 +1,25 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+
+  static classes = ["active"]
+
+  static targets = ["tab", "panel"]
+
+  switch(e) {
+    const active = this.tabTargets.filter(elem => elem.classList.contains("active-tab") )[0]
+    const current = e.target.parentElement
+
+    if (current !== active) {
+      this.tabTargets.forEach((tab, index) => {
+        if (tab === active) {
+          tab.classList.remove(...this.activeClasses)
+          this.panelTargets[index].classList.add("hidden")
+        } else if (tab === current) {
+          current.classList.add(...this.activeClasses)
+          this.panelTargets[index].classList.remove("hidden")
+        }
+      })
+    }
+  }
+}
