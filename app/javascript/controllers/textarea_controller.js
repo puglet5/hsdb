ea8483a9a13c8f181a58c50886655b62bc2655e1
Michael Basmanov <puglet5@mail.ru>
2022-10-12 01:31:53 +0300

    cleanup js controllers

--- app/javascript/controllers/textarea_controller.js
+++ app/javascript/controllers/textarea_controller.js
@@ -1,4 +1,4 @@
-import {Controller} from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = ["input"]
@@ -8,9 +8,9 @@ export default class extends Controller {
         this.inputTarget.style.minHeight = `${this.inputTarget.scrollHeight}px`
     }
 
-    resize(event){
+    resize(event) {
         event.target.style.height = "5px"
-        event.target.style.height =  `${event.target.scrollHeight}px`
+        event.target.style.height = `${event.target.scrollHeight}px`
     }
 
 }
