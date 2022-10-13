1330e999e019e74553fbab72fe8d51a82f5e5bee
Michael Basmanov <puglet5@mail.ru>
2022-10-14 01:52:10 +0300

    sort tailwind classes

--- app/javascript/controllers/metadataForm_controller.js
+++ app/javascript/controllers/metadataForm_controller.js
@@ -34,7 +34,7 @@ export default class extends Controller {
     addButton.classList.remove("mt-9")
     addButton.textContent = ""
     // eslint-disable-next-line quotes
-    addButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M18 12H6" /></svg>`
+    addButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M18 12H6" /></svg>`
     addButton.addEventListener("click", function () { this.parentNode.remove() }, false)
 
     let childInputs = this.formContainerTarget.querySelectorAll("input.metadata")
