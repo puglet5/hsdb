93f88e924123bd1b4e6e50034f74ee9cdda13217
Michael Basmanov <puglet5@mail.ru>
2022-10-25 19:29:26 +0300

    bump, lint

--- db/rsdb_migrate/20221025152016_add_range_to_spectrum_files.rb
+++ db/rsdb_migrate/20221025152016_add_range_to_spectrum_files.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddRangeToSpectrumFiles < ActiveRecord::Migration[7.0]
   def change
     add_column :spectrum_files, :range, :integer, null: false, default: 0
