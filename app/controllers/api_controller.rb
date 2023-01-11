0c8766011fad31f995e7d5d7dd937eb87051948b
Michael Basmanov <puglet5@mail.ru>
2023-01-11 22:10:40 +0300

    revert auth skip for api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ApiController < ApplicationController
-  # before_action :doorkeeper_authorize!
+  before_action :doorkeeper_authorize!
   skip_before_action :authenticate_user!
   skip_before_action :verify_authenticity_token
   respond_to :json
