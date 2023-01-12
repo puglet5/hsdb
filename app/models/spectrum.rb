d815fe4840637501ea5a880e1014b20ad1236edf
Michael Basmanov <puglet5@mail.ru>
2023-01-13 01:49:43 +0300

    change spectrum status enum

--- app/models/spectrum.rb
+++ app/models/spectrum.rb
@@ -6,7 +6,7 @@
 #
 #  id         :bigint           not null, primary key
 #  format     :integer          default("not_set"), not null
-#  status     :integer          default("none"), not null
+#  status     :integer          default("raw"), not null
 #  category   :integer          default("not_set"), not null
 #  sample_id  :bigint           not null
 #  created_at :datetime         not null
@@ -21,7 +21,7 @@ class Spectrum < RsdbRecord
 
   belongs_to :sample, inverse_of: :spectra, touch: true
 
-  enum status: { none: 0, successful: 1, pending: 2, ongoing: 3, error: 4, mixed: 5 }, _prefix: :processing, _default: :none
+  enum status: { raw: 0, successful: 1, pending: 2, ongoing: 3, error: 4, other: 5 }, _prefix: :processing, _default: :raw
   enum range: { not_set: 0, vis: 1, ir: 2, uv: 3, other: 4 }, _default: :ir
   enum format: { not_set: 0, csv: 1, imp: 2, spectable: 3, mon: 4, txt: 5, dat: 6, other: 7 }, _default: :not_set, _suffix: :format
   enum category: { not_set: 0 }, _default: :not_set, _suffix: :category
--- app/serializers/spectrum_serializer.rb
+++ app/serializers/spectrum_serializer.rb
@@ -1,5 +1,19 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: spectra
+#
+#  id         :bigint           not null, primary key
+#  format     :integer          default("not_set"), not null
+#  status     :integer          default("raw"), not null
+#  category   :integer          default("not_set"), not null
+#  sample_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  range      :integer          default("ir"), not null
+#  metadata   :jsonb            not null
+#
 class SpectrumSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
