e69c5fa8f2163c4bf457d3f78e482890c8bd1ad4
Michael Basmanov <puglet5@mail.ru>
2023-01-14 17:58:17 +0300

    add create action for spectrum api

--- app/controllers/api/v1/spectra_controller.rb
+++ app/controllers/api/v1/spectra_controller.rb
@@ -12,6 +12,24 @@ module Api
         @spectrum = Spectrum.find_by(id: params[:id]) or not_found
         render json: @spectrum
       end
+
+      def create
+        @spectrum = Spectrum.new(spectrum_params)
+
+        if @spectrum.save!
+          render json: @spectrum, status: :ok
+        else
+          render json: { errors: @spectrum.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def spectrum_params
+        params.require(:spectrum).permit(
+          :sample_id, :format, :status, :category, :range, :file
+        )
+      end
     end
   end
 end
