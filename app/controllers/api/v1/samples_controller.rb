c5e57149a63de473434b47137b9f72c7f383f8a2
Michael Basmanov <puglet5@mail.ru>
2023-01-14 18:06:12 +0300

    add create action for samples api

--- app/controllers/api/v1/samples_controller.rb
+++ app/controllers/api/v1/samples_controller.rb
@@ -12,6 +12,39 @@ module Api
         @sample = Sample.find_by(id: params[:id]) or not_found
         render json: @sample
       end
+
+      def create
+        @sample = current_user.samples.build(sample_params)
+
+        if @sample.save!
+          render json: @sample, status: :ok
+        else
+          render json: { errors: @sample.errors }, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def sample_params
+        params.require(:sample).permit(
+          :title,
+          :metadata,
+          :category,
+          :origin,
+          :owner,
+          :description,
+          :lock_version,
+          :sku,
+          :color,
+          :formula,
+          :cas_no,
+          :cas_name,
+          :survey_date,
+          spectra_attributes: %i[id file],
+          images: [],
+          documents: []
+        )
+      end
     end
   end
 end
