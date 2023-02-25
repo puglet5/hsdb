cfad2bd48aea748b52e10df4cf9b328d1516ded5
Michael Basmanov <puglet5@mail.ru>
2023-02-26 00:07:51 +0300

    set task_id after receiving sdp response

--- app/services/processing_request_sender.rb
+++ app/services/processing_request_sender.rb
@@ -14,10 +14,13 @@ class ProcessingRequestSender < ApplicationService
   end
 
   def call
+    return unless @record
+
     request_url = "#{URL}/processing/#{@record_id}?record_type=#{record_type}"
+    @record.processing_pending!
     begin
-      response = Faraday.post(request_url)
-      handle_response(response)
+      response = Faraday.post request_url
+      handle_response response
     rescue Faraday::ConnectionFailed => e
       Rails.logger.error("Server connection failed for #{@record_type} with id #{@record_id}: #{e}")
       @record.processing_error!
@@ -27,8 +30,19 @@ class ProcessingRequestSender < ApplicationService
   private
 
   def handle_response(response)
-    return unless response.status != 202
+    unless response.success?
+      @record.processing_error!
+      return
+    end
 
-    @record.processing_ongoing!
+    response_body = JSON.parse response.body
+    Rails.logger.info response_body
+
+    task_id = response_body['task']['id']
+    update_task_id task_id
+  end
+
+  def update_task_id(id)
+    @record.update! task_id: id
   end
 end
