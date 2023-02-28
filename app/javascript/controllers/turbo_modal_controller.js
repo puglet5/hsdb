ccf878eb922e3f3ea7340505a830a31da4d8e0e6
Michael Basmanov <puglet5@mail.ru>
2023-02-28 06:33:20 +0300

    fix: prevent esc hotkey from closing modal when uppy is opened

--- app/javascript/controllers/turbo_modal_controller.js
+++ app/javascript/controllers/turbo_modal_controller.js
@@ -6,10 +6,17 @@ export default class extends Controller {
 
   static targets = ["modal"]
 
+  uppy_db_arr = undefined
+  uppy_states_arr = undefined
+
   connect() {
     hotkeys("escape", (event) => {
-      event.preventDefault()
-      this.close()
+      this.uppy_db_arr = document.getElementsByClassName("uppy-Dashboard")
+      this.uppy_states_arr = [...this.uppy_db_arr].map(e => e.getAttribute("aria-hidden") == "true")
+      if (!this.uppy_states_arr.includes(false)) {
+        event.preventDefault()
+        this.close()
+      } else return
     })
     document.body.classList.add("overflow-hidden")
   }
@@ -18,5 +25,4 @@ export default class extends Controller {
     this.modalTarget.remove()
     document.body.classList.remove("overflow-hidden")
   }
-
 }
