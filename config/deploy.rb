3cc63a599ee3f6b2e491825b15d253561ceac063
Michael Basmanov <puglet5@mail.ru>
2023-07-04 08:37:29 +0000

    Production (#142)
    * rebase: to master
    
    * fix: treat postcss conifg as commonjs
    
    * fix[ci]: build scripts in ci
    
    * fix: ruby version in deploy
    
    * fix: stimulus value names for uploads controller
    
    * fix: storage config for production
    
    * fix: make prod storage public
    
    * fix: change storage service in prod env
    
    * fix: place shared storage one dir up
    
    * fix: ruby version in deploy
    
    * fix: stimulus value names for uploads controller
    
    * fix: storage config for production
    
    * fix: make prod storage public
    
    * fix: change storage service in prod env
    
    * fix: place shared storage one dir up

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,7 +13,7 @@ set :user, 'hslab'
 set :puma_threads,    [4, 16]
 set :puma_workers,    0
 
-set :rvm_ruby_version, '3.2.1'
+set :rvm_ruby_version, '3.2.2'
 
 set :pty,             true
 set :use_sudo,        false
