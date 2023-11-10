99d4e88497d016d5193fa0803b20c160b693caa8
Michael Basmanov <puglet5@mail.ru>
2023-11-11 01:35:05 +0300

    fix: remove redundant elasticsearch initializer

--- spec/support/elasticsearch.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'webmock/rspec'
-
-RSpec.configure do |config|
-  config.before :each do
-    stub_request(:any, /localhost:9200/).to_return(status: 200)
-  end
-end
