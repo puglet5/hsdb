69858b493afe7d9cb5033dbf98c52d6a1c1368bf
Michael Basmanov <basmanov@itmo.ru>
2024-08-15 21:15:09 +0300

    chore: lint

--- app/services/guest_user.rb
+++ app/services/guest_user.rb
@@ -9,15 +9,15 @@ class GuestUser
     false
   end
 
-  def method_missing(name, *args, &block)
+  def method_missing(name, ...)
     return false if name.to_s.end_with?('_role?')
 
-    super(name, *args, &block)
+    super
   end
 
   def respond_to_missing?(name, include_private)
     return true if name.to_s.end_with?('_role?')
 
-    super(name, include_private)
+    super
   end
 end
--- config/initializers/mini-profiler.rb
+++ config/initializers/mini-profiler.rb
@@ -5,7 +5,7 @@ Rack::MiniProfiler.config.position = 'bottom-left'
 
 module RackTurboExtension
   def get_profile_script(*args, **kargs)
-    super(*args, **kargs).gsub('></script>', ' data-turbo-permanent="true"></script>')
+    super.gsub('></script>', ' data-turbo-permanent="true"></script>')
   end
 end
 
